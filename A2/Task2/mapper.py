a5258f1719bd52725e844e004c9e17207c788461
Kshitij Prit Gopali <56757115+gopali24@users.noreply.github.com>
2021-10-16 22:25:36 +0530

    Create mapper.py

---
+++ A2/Task2/mapper.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+def modp(li):
+    sum=0
+    for i in li:
+        sum+=i*i
+    return sum**0.5
+import sys 
+import json
+l=[]
+f1=open('output.txt','r')
+f2=open('embedding-sample.json','r')
+y=json.loads(f2.read())
+
+for line in f1.readlines():
+    line=line.strip()
+    key,val=line.split(' ',1)
+    val=val[1:-1]
+    if ',' in val:    vall=list(map(int,val.split(',')))
+    else: vall=[int(val)]
+    l=len(vall)
+    for i in vall:
+        #print(key,i,1/l)
+        dp=sum([ (y[str(key)][k]*y[str(i)][k]) for k in range(0,5)])
+        temp=dp/(modp(y[str(key)])*(modp(y[str(i)])))
+        print(1/l,temp)
+        print(key,i,temp*(1/l))
+        #print(dp)        
+                
+'''
+for i in y:
+    for j in y:
+        dp=sum([ (y[i][k]*y[j][k]) for k in range(0,5)])
+        temp=dp/(modp(y[i])*(modp(y[j])))
+        #print(i,j,round(temp,4)) '''
