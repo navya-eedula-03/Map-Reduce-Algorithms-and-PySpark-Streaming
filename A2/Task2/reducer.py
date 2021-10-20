e4aac4ce8e4b9d044ec1c49ed4008daa7a0e82a2
Kshitij Prit Gopali <56757115+gopali24@users.noreply.github.com>
2021-10-20 21:13:29 +0530

    Update reducer.py

--- A2/Task2/reducer.py
+++ A2/Task2/reducer.py
@@ -7,7 +7,6 @@ for line in sys.stdin:
     line = line.strip()
     key1,key2,value =line.split(' ',2)
     key1,key2,value=int(key1),int(key2),float(value)
-    # print(key1,"\t",key2,"\t",value)
     if key2 in Dict2:
         Dict2[key2]+=0.85*value
     else:
@@ -16,6 +15,5 @@ for line in sys.stdin:
 f1=open('V1.txt','w+')
 for key in Dict2:
     Dict2[key]+=0.15
-    # print(key,",",round(Dict2[key],2))
     print("%d,%1.2f "%(key,Dict2[key]))
     f1.write("%d,%1.2f\n"%(key,Dict2[key]))
