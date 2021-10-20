fd7766aab7dc74d559563cb9af51b41ed4d298fc
Kshitij Prit Gopali <56757115+gopali24@users.noreply.github.com>
2021-10-20 21:12:59 +0530

    Using args

--- A2/Task2/mapper.py
+++ A2/Task2/mapper.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import sys
 
 def modp(li):
     sum=0
@@ -8,11 +9,14 @@ def modp(li):
 import sys 
 import json
 l=[]
-f1=open('output.txt','r')
-f2=open('embedding-sample.json','r')
+emb_fi = sys.argv[1]
+v_fi = sys.argv[2]
+f2=open(emb_fi,'r')
+f1=open(v_fi,'r')
+
 y=json.loads(f2.read())
 
-for line in f1.readlines():
+for line in sys.stdin:
     line=line.strip()
     key,val=line.split(' ',1)
     val=val[1:-1]
@@ -20,10 +24,6 @@ for line in f1.readlines():
     else: vall=[int(val)]
     l=len(vall)
     for i in vall:
-        #print(key,i,1/l)
         dp=sum([ (y[str(key)][k]*y[str(i)][k]) for k in range(0,5)])
         temp=dp/(modp(y[str(key)])*(modp(y[str(i)])))
-        #print(1/l,temp)
         print(key,i,temp*(1/l))
-        #print(dp)        
-
