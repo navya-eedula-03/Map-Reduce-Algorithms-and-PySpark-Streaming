277bf4ff10dbebfaf85f70733568c869afef41f9
Kshitij Prit Gopali <56757115+gopali24@users.noreply.github.com>
2021-10-23 10:38:06 +0530

    final mapper

--- A2/Task2/mapper.py
+++ A2/Task2/mapper.py
@@ -6,6 +6,7 @@ def modp(li):
     for i in li:
         sum+=i*i
     return sum**0.5
+
 import sys 
 import json
 l=[]
@@ -20,10 +21,17 @@ for line in sys.stdin:
     line=line.strip()
     key,val=line.split(' ',1)
     val=val[1:-1]
-    if ',' in val:    vall=list(map(int,val.split(',')))
-    else: vall=[int(val)]
+    if ',' in val:
+        vall=list(map(int,val.split(',')))
+    else:
+        vall=[int(val)]
     l=len(vall)
     for i in vall:
         dp=sum([ (y[str(key)][k]*y[str(i)][k]) for k in range(0,5)])
         temp=dp/(modp(y[str(key)])*(modp(y[str(i)])))
         print(key,i,temp*(1/l))
+
+for line in f1.readlines():
+    line=line.strip()
+    key,val=line.split(',')
+    print(0,key,0)
