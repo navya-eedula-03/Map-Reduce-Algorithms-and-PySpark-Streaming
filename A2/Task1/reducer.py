11fac958844163a74224ecaf7d6fe9c2d4bac53b
Kshitij Prit Gopali <56757115+gopali24@users.noreply.github.com>
2021-10-23 10:36:50 +0530

    updated reducer to take all pages inot v

--- A2/Task1/reducer.py
+++ A2/Task1/reducer.py
@@ -7,17 +7,22 @@ path = sys.argv[1]
 
 f1=open(path,'w+')
 
+
+a=set()
 for line in sys.stdin:
     line=line.strip()
     key,val=line.split(' ')
     key,val=int(key),int(val)
+    a.add(key)
+    a.add(val)
     if key in Dict:
         Dict[key].append(val)
     else:
         Dict[key]=[val]
 
-for i in Dict:
-    print(i,Dict[i])
+for i in a:
+    if( i in Dict):
+        print(i,Dict[i])
     f1.write(str(i)+",1\n")
 
 f1.close()
